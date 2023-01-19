321ba423c9e644d63fb1f84c420613ffc587d960
Bill Lan <bilan604gm@gmail.com>
2023-01-18 20:02:38 -0800

    Initial Commit

---
+++ main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+class MyClass {
+    public static void main(String args[]) {
+        ConnectFour game = new ConnectFour();
+        boolean activePlayer = true;
+        Scanner sc = new Scanner(System.in);
+        game.viewBoard();
+        System.out.println(game.getBoard());
+        while (!game.over()) {
+            game.play(activePlayer, sc);
+            activePlayer = !activePlayer;
+        }
+        System.out.println("Game Over :(");
+        sc.close();
+    }
+}
